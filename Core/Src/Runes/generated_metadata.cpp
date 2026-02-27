0a3768ffbba015402738f73fac82b6b2d72cfa49
victhor <victorlopezcortes25@gmail.com>
2026-02-27 18:34:21 +0100

    change commit of JSON_ADE

--- Core/Src/Runes/generated_metadata.cpp
+++ Core/Src/Runes/generated_metadata.cpp
@@ -5,11 +5,11 @@
 extern "C"{
     const char DESCRIPTION[255]  __attribute__((section(".metadata_pool")))=
         "****************"  // placeholder for beggining
-        "20260227T180038"   // DateTime using ISO-8601 format
+        "20260227T183410"   // DateTime using ISO-8601 format
         " "                 // alignment
-        "16efa89a"          // STLIB commit
+        "0375ab4b"          // STLIB commit
         "--------"          // ADJ commit
-        "7b08a67f"          // Board commit
+        "3711fe5c"          // Board commit
         // the '=' is used for unparsing
         ;
 }
